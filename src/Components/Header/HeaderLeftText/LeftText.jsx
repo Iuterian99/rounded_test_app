6340fdf17eeb54ab99e6c8f2aee248def1fb03fd
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-04-25 23:42:06 +0500

    finished text part

--- src/Components/Header/HeaderLeftText/LeftText.jsx
+++ src/Components/Header/HeaderLeftText/LeftText.jsx
@@ -2,10 +2,14 @@ import "./LeftText.scss";
 const LeftText = () => {
   return (
     <div className="leftText">
-      <h1 className="heading1">
+      <h1 className="leftText__heading1">
         We help you <br /> grow your business{" "}
         <span className="faster">faster</span>
       </h1>
+      <p className="leftText__paragraph">
+        Ehya is the Instagram analytics platform teams use to stay focused on
+        the goals, track engagement for report your business .
+      </p>
     </div>
   );
 };
